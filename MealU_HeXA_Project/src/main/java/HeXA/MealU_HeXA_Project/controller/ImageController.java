23579fea92a92e6546b67630f662d68449f918b8
eumppe <pych05@unist.ac.kr>
2023-09-11 16:37:35 +0900

    notFancyAdminBranch

--- MealU_HeXA_Project/src/main/java/HeXA/MealU_HeXA_Project/controller/ImageController.java
+++ MealU_HeXA_Project/src/main/java/HeXA/MealU_HeXA_Project/controller/ImageController.java
@@ -3,6 +3,7 @@ package HeXA.MealU_HeXA_Project.controller;
 import HeXA.MealU_HeXA_Project.service.ImageUrlsService;
 import HeXA.MealU_HeXA_Project.service.dto.ImageUrlsDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,25 +18,40 @@ public class ImageController {
 
     private ImageUrlsService imageUrlsService;
 
+    @Value("${secretKey}")
+    String secretKey;
+
     @Autowired
     public ImageController(ImageUrlsService imageUrlsService) {
         this.imageUrlsService = imageUrlsService;
     }
 
     @PostMapping("/imageDormitoryUrlImport")
-    public ResponseEntity<Void> importDormitoryImageUrl(String dormitoryUrl) throws URISyntaxException {
+    public ResponseEntity<Void> importDormitoryImageUrl(String dormitoryUrl, String theKey) throws URISyntaxException {
+        if (!theKey.equals(secretKey)) {
+            return ResponseEntity.badRequest().build();
+        }
+
         imageUrlsService.importDormitoryUrl(dormitoryUrl);
         return ResponseEntity.created(new URI("/importSuccess")).build();
     }
 
     @PostMapping("/imageStudentUrlImport")
-    public ResponseEntity<Void> importStudentImageUrl(String studentUrl) throws URISyntaxException {
+    public ResponseEntity<Void> importStudentImageUrl(String studentUrl, String theKey) throws URISyntaxException {
+        if (!theKey.equals(secretKey)) {
+            return ResponseEntity.badRequest().build();
+        }
+
         imageUrlsService.importStudentUrl(studentUrl);
         return ResponseEntity.created(new URI("/importSuccess")).build();
     }
 
     @PostMapping("/imageProfessorUrlImport")
-    public ResponseEntity<Void> importProfessorImageUrl(String professorUrl) throws URISyntaxException {
+    public ResponseEntity<Void> importProfessorImageUrl(String professorUrl, String theKey) throws URISyntaxException {
+        if (!theKey.equals(secretKey)) {
+            return ResponseEntity.badRequest().build();
+        }
+
         imageUrlsService.importProfessorUrl(professorUrl);
         return ResponseEntity.created(new URI("/importSuccess")).build();
     }
